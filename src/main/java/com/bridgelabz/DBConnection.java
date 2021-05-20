c33b54f6f241697bbca801fa4f5983dfd671c99e
VinayakKamat95 <omkarkamat1995@gmail.com>
2021-05-20 08:57:03 +0530

    To create a payroll service database and have java program connect to database

--- src/main/java/com/bridgelabz/DBConnection.java
+++ src/main/java/com/bridgelabz/DBConnection.java
@@ -1,7 +1,30 @@
 package com.bridgelabz;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
 public class DBConnection {
-    public static void main(String[] args) {
-        System.out.println("Welcome to JDBC Employee Payroll ");
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+        String userName = "root";
+        String password = "root";
+        Connection connection;
+        Class.forName("com.mysql.jdbc.Driver");
+        System.out.println("Driver loaded!!!");
+        listDrivers();
+        System.out.println("Connecting to database:"+jdbcURL);
+        connection = DriverManager.getConnection(jdbcURL, userName, password);
+        System.out.println("Connection is successful!!!"+connection);
+    }
+
+    private static void listDrivers() {
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while (driverList.hasMoreElements()){
+            Driver driverClass = driverList.nextElement();
+            System.out.println(" "+driverClass.getClass().getName());
+        }
     }
 }
